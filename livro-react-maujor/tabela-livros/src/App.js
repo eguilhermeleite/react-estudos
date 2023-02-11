5dae15b838eb95906dccd44f4468a21623022679
Edvaldo Leite <eguilhermeleite@gmail.com>
2023-02-11 00:29:18 -0300

    update App.js e criado componente TabelaFoot.js

--- livro-react-maujor/tabela-livros/src/App.js
+++ livro-react-maujor/tabela-livros/src/App.js
@@ -1,16 +1,31 @@
 //components
-import React from "react";
+import React, { Component } from "react";
 import TabelaBody from "./components/TabelaBody";
+import TabelaFoot from "./components/TabelaFoot";
 import TabelaHead from "./components/TabelaHead";
 
+class App extends Component {
+  render() {
+    return (
+      <table className="tabela">
+        <TabelaHead />
+        <TabelaBody />
+        <TabelaFoot />
+      </table>
+    );
+  }
+}
+
+/*
 const App = () => (
   <div>
     <table className="tabela">
       <TabelaHead />
       <TabelaBody />
+      <TabelaFoot />
     </table>
   </div>
 );
-
+*/
 
 export default App;
---
+++ livro-react-maujor/tabela-livros/src/components/TabelaFoot.js
@@ -0,0 +1,10 @@
+
+const TabelaFoot = () => (
+    <tfoot>
+        <tr>
+            <th colSpan="4">Rodapé da tabela</th>
+        </tr>
+    </tfoot>
+);
+
+export default TabelaFoot;
