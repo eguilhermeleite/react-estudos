dbc3cdeb4a1c26063427f7d496f56f6e2d8ad075
Edvaldo G Leite <eguilhermeleite@gmail.com>
2023-03-05 00:28:21 -0300

    Altera

--- curso-react-udemy/REQ-HTTP/request-http/src/App.js
+++ curso-react-udemy/REQ-HTTP/request-http/src/App.js
@@ -1,10 +1,32 @@
 
-import './App.css';
+
+import { useState, useEffect } from 'react';
+import Table from './components/Table';
+
+const url = "http://localhost:3000/products";
+
 
 function App() {
+
+  // armazena o estado dos produtos
+  const [products, setProducts] = useState([]);
+
+  // faz a requisicao ao dados
+  useEffect(() => {
+    async function fetchData() {
+      const response = await fetch(url);
+      const data = await response.json();
+      setProducts(data);
+    }
+    fetchData();
+
+  }, []);// fim de useEffect
+
+
   return (
-    <div className="App">
-     <h1>Lista de produtos</h1>
+    <div className="container">
+      <h1>Lista de produtos</h1>
+      <Table products={products} />
     </div>
   );
 }
---
+++ curso-react-udemy/REQ-HTTP/request-http/src/components/Table.js
@@ -0,0 +1,30 @@
+
+
+const Table = ({products}) => {
+    return (
+        <div>
+            <table className="table">
+                <thead>
+                    <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">NOME</th>
+                        <th scope="col">Preço</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {products.map((product) => (
+                        <tr key={product.id}>
+                            <td>{product.id}</td>
+                            <td>{product.name}</td>
+                            <td>{product.price}</td>
+                        </tr>
+                    ))}
+
+                </tbody>
+            </table>
+
+        </div>
+    )
+}
+
+export default Table
